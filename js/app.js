77564149155724fc411655e61a9c120122f895d5
corbiobio <edarnand@edenschool.fr>
2022-09-23 15:06:56 +0200

    condition

--- js/app.js
+++ js/app.js
@@ -31,8 +31,25 @@ const identity = "Bonjour" + " " + prenom + " " + "vous avez" + " " + age + " " 
 
 majuscule = identity.toUpperCase() ;
 
-console.log(identity) // l'adition de tout les string + la variable prenom et age
-console.log(majuscule) // on a pris le texte dan idenity pour le passer en maj
+console.log(identity); // l'adition de tout les string + la variable prenom et age
+console.log(majuscule); // on a pris le texte dan idenity pour le passer en maj
 
+age = prompt();
 
+if (age<=0){
+    console.log("vous etes un poisson (pane)");
+}else if (age<=120){
+    console.log("Votre age est valide");
+}else{
+    console.log("je ne suit pas daccord");
+}
+
+if (age>=21 || age ){
+    console.log("vous etes majeur")
+}else if(age<21 && age>=18){
+    console.log("vous etes majeur mais pas au usa")
+}else {
+    console.log("vous etes mineur")
+}
+// && || !
 

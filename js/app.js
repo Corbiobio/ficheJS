6b2833e0ed5ee21bad5902cbdd5999d4a69559c2
corbiobio <edarnand@edenschool.fr>
2022-09-23 14:44:27 +0200

    type de variable et operation sur les vavriables

--- js/app.js
+++ js/app.js
@@ -1,10 +1,38 @@
-console.log("hello")
-let age
+console.log("hello");
+let age;
 age = 16 //number 
-console.log(typeof age)
+console.log(typeof age);
 
 const prenom = "elevan" //string
-console.log(typeof prenom)
+console.log(typeof prenom);
+
+let addition = 4+12;
+console.log(addition + " addition");
+
+let soustraction = 2-50;
+console.log(soustraction + " soustraction");
+
+let multiplication = 3*8 ;
+console.log(multiplication + " multiplication");
+
+let division = 7/2 ;
+console.log(division + " division");
+
+let modulo = 5%2 ;
+console.log(modulo = " modulo");
+
+let test = 3>5
+console.log(test + " test"); //booleen
+
+// > >= < <= === == != !==
+
+const identity = "Bonjour" + " " + prenom + " " + "vous avez" + " " + age + " " + "ans";
+//bonjour + elevan + vous avez + 16 + ans 
+
+majuscule = identity.toUpperCase() ;
+
+console.log(identity) // l'adition de tout les string + la variable prenom et age
+console.log(majuscule) // on a pris le texte dan idenity pour le passer en maj
 
 
 
